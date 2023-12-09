5de4487c884049b889ab3274eeb66ab061ebc5c9
DBCOOPER <reddyindr53@gmail.com>
2023-12-09 19:58:10 +0530

    testingggggggghhhhhh

--- src/app/fa811077-d0df-484f-9f8d-954c2add8d11/[id]/page.js
+++ src/app/fa811077-d0df-484f-9f8d-954c2add8d11/[id]/page.js
@@ -1,7 +1,7 @@
 "use client"
 import React, { useEffect, useState } from "react"
 import "./src.css"
-export default function hello({ params }) {
+export default function Hello({ params }) {
  const [data, setData] = useState(null)
 
  const getData = async (params) => {
